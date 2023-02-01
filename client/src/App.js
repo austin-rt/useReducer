af96dfb034c379d014a27a8d6b0c435a263241c4
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-02-01 12:14:40 -0500

    refactor color

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,26 @@ function App() {
   const reducer = (state, action) => {
     switch (action.type) {
       case 'increment':
-        return { count: state.count + 1 };
+        return { ...state, count: state.count + 1 };
       case 'decrement':
-        return { count: state.count - 1 };
+        return { ...state, count: state.count - 1 };
+      case 'input':
+        return { ...state, input: action.payload };
+      case 'changeColor':
+        return { ...state, color: action.payload };
       default:
         return state;
     }
   };
 
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-  const [input, setInput] = useState('');
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    input: '',
+    color: 'white'
+  });
 
   const handleChange = e => {
-    setInput(e.target.value);
+    dispatch({ action: { payload: e.target.value } });
   };
 
   const incrementCount = () => {
@@ -28,15 +34,33 @@ function App() {
     dispatch({ type: 'decrement' });
   };
 
+  const changeColor = e => {
+    dispatch({ type: 'changeColor', payload: e.target.name });
+  };
+
   return (
-    <main>
+    <main className={state.color}>
       <section>
         <input
           type='text'
-          value={input}
+          value={state.input}
           onChange={handleChange}
         />
-        <p className='output'>{input || 'output'}</p>
+        <p className='output'>{state.input || 'output'}</p>
+        <div className='button-container'>
+          <button
+            onClick={changeColor}
+            name='react-blue'
+          >
+            react blue
+          </button>
+          <button
+            onClick={changeColor}
+            name=''
+          >
+            white
+          </button>
+        </div>
       </section>
       <section>
         <p className='output'>{state.count}</p>
