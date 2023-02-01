c3de039b7f2a98369f79c3e62f4ffd9096780c00
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-02-01 10:26:43 -0500

    add react app

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ![](./assets/png/useReducer-header-small.png)
 
-In this article, we will explore when and how to use React’s `useLayoutEffect`
-hook and how it relates to the `useEffect` Hook.
+In this article, we will explore when and how to use React’s `useReducer` hook
+and how it relates to the `useState` Hook.
 
 If you'd like to skim this as a Medium article, it can be found [here](#)
 
@@ -13,3 +13,7 @@ If you'd like to skim this as a Medium article, it can be found [here](#)
 - `cd client`
 - `npm i`
 - `npm start`
+
+## useReducer
+
+`useReducer` is for managing complex state logic using the Redux pattern.
---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+function App() {
+  const [input, setInput] = useState('');
+  const [count, setCount] = useState(0);
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
+  const incrementCount = () => {
+    setCount(prev => prev + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(prev => prev - 1);
+  };
+
+  return (
+    <main>
+      <section>
+        <input
+          type='text'
+          value={input}
+          onChange={handleChange}
+        />
+        <p className='output'>{input || 'output'}</p>
+      </section>
+      <section>
+        <p className='output'>{count}</p>
+        <div className='button-container'>
+          <button onClick={incrementCount}>increment</button>
+          <button onClick={decrementCount}>decrement</button>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
