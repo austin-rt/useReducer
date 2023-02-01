97731f6d7588cfef2fb459b9c0032ab1c8e3443c
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-02-01 10:47:00 -0500

    define reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import { useState, useReducer } from 'react';
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'increment':
+        return { count: state.count + 1 };
+      case 'decrement':
+        return { count: state.count - 1 };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   const [input, setInput] = useState('');
   const [count, setCount] = useState(0);
 
